04b188e1eaaf9976950c1117608308b603e2548a
Paloma <paloma.sanchezc@icloud.com>
2025-05-20 14:43:17 +0200

    Description de vos modifications

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,7 @@ export default function App()
 {
     return(
         <>
-            <a-scene mindar-image="imageTargetSrc: /ARimagebased/targets.mind;" vr-mode-ui="enabled: false" device-orientation-permission-ui="enabled: false">
+            <a-scene mindar-image="imageTargetSrc: /ARimagebased/targets-compteur.mind;" vr-mode-ui="enabled: false" device-orientation-permission-ui="enabled: false">
                 <a-camera position="0 0 0" look-controls="enabled: false"></a-camera>
                 <a-entity mindar-image-target="targetIndex: 0">
                     <a-plane color="blue" opaciy="0.5" position="0 0 0" height="0.552" width="1" rotation="0 0 0"></a-plane>
