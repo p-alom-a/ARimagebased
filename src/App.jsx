31ef59aa57686a047a80b493f35a6c01c90359df
Paloma <paloma.sanchezc@icloud.com>
2025-05-20 14:59:18 +0200

    Description de vos modifications

--- src/App.jsx
+++ src/App.jsx
@@ -9,8 +9,7 @@ export default function App()
             <a-scene mindar-image="imageTargetSrc: /ARimagebased/targets-compteur.mind;" vr-mode-ui="enabled: false" device-orientation-permission-ui="enabled: false">
                 <a-camera position="0 0 0" look-controls="enabled: false"></a-camera>
                 <a-entity mindar-image-target="targetIndex: 0">
-                    <a-box color="blue" position="0.1 0 0" width="0.02" height="0.02" depth="0.02"></a-box>
-                    <a-box color="green" position="0 0.1 0" width="0.02" height="0.02" depth="0.02"></a-box>
+                    <a-box color="blue" position="-0.200074  0.147799  0.086576" width="0.1" height="0.1" depth="0.02"></a-box>
                 </a-entity>
             </a-scene>
         </>
