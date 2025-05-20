df22dda2af9b3f029cd226ac2ef29678e1ba9883
Paloma <paloma.sanchezc@icloud.com>
2025-05-20 15:52:09 +0200

    Description de vos modifications

--- src/App.jsx
+++ src/App.jsx
@@ -1,37 +1,41 @@
-import React, { useState } from 'react';
 import 'aframe';
 import 'mind-ar/dist/mindar-image-aframe.prod.js';
+import { useState } from 'react';
 
-export default function App() {
-    const [isRedCubeVisible, setRedCubeVisible] = useState(false);
+export default function App()
+{
+    const [showRedCube, setShowRedCube] = useState(false);
 
-    const toggleRedCube = () => {
-        setRedCubeVisible(!isRedCubeVisible);
+    const handleCubeClick = () => {
+        setShowRedCube(true);
     };
 
-    return (
+    return(
         <>
             <a-scene mindar-image="imageTargetSrc: /ARimagebased/targets-compteur.mind;" vr-mode-ui="enabled: false" device-orientation-permission-ui="enabled: false">
                 <a-camera position="0 0 0" look-controls="enabled: false"></a-camera>
                 <a-entity mindar-image-target="targetIndex: 0">
-                    <a-box
-                        color="blue"
-                        position="-0.200074 0.147799 0.086576"
-                        width="0.1"
-                        height="0.1"
+                    <a-box 
+                        color="blue" 
+                        position="-0.200074  0.147799  0.086576" 
+                        width="0.1" 
+                        height="0.1" 
                         depth="0.02"
-                        onclick={toggleRedCube}
+                        events={{
+                            click: handleCubeClick
+                        }}
                     ></a-box>
-                    <a-box
-                        color="red"
-                        position="0.200074 0.147799 0.086576"
-                        width="0.1"
-                        height="0.1"
-                        depth="0.02"
-                        visible={isRedCubeVisible}
-                    ></a-box>
+                    {showRedCube && (
+                        <a-box 
+                            color="red" 
+                            position="-0.200074 0.247799 0.086576" 
+                            width="0.1" 
+                            height="0.1" 
+                            depth="0.02"
+                        ></a-box>
+                    )}
                 </a-entity>
             </a-scene>
         </>
-    );
+    )
 }
