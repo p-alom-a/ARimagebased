9790af6af13eb84e4fbf7ce993ad5519c373adc1
Paloma <paloma.sanchezc@icloud.com>
2025-05-20 16:24:39 +0200

    Description de vos modifications

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,36 @@
 import React, { useEffect, useRef } from 'react';
-import {MindARThree} from './mind-ar/dist/mindar-image-three.prod.js';
 import * as THREE from 'three';
+import { MindARThree } from './mindar-image-three.prod.js';
 
 export default function App() {
     const containerRef = useRef(null);
 
-  useEffect(() => {
-    const mindarThree = new MindARThree({
-      container: containerRef.current,
-      imageTargetSrc: "https://cdn.jsdelivr.net/gh/hiukim/mind-ar-js@1.2.0/examples/image-tracking/assets/card-example/card.mind"
-    });
-    const {renderer, scene, camera} = mindarThree;
-    const anchor = mindarThree.addAnchor(0);
-    const geometry = new THREE.PlaneGeometry(1, 0.55);
-    const material = new THREE.MeshBasicMaterial( {color: 0x00ffff, transparent: true, opacity: 0.5} );
-    const plane = new THREE.Mesh( geometry, material );
-    anchor.group.add(plane);
+    useEffect(() => {
+        const mindarThree = new MindARThree({
+            container: containerRef.current,
+            imageTargetSrc: "https://cdn.jsdelivr.net/gh/hiukim/mind-ar-js@1.2.0/examples/image-tracking/assets/card-example/card.mind"
+        });
 
-    mindarThree.start();
-    renderer.setAnimationLoop(() => {
-      renderer.render(scene, camera);
-    });
+        const {renderer, scene, camera} = mindarThree;
+        const anchor = mindarThree.addAnchor(0);
+        const geometry = new THREE.PlaneGeometry(1, 0.55);
+        const material = new THREE.MeshBasicMaterial( {color: 0x00ffff, transparent: true, opacity: 0.5} );
+        const plane = new THREE.Mesh( geometry, material );
+        anchor.group.add(plane);
 
-    return () => {
-      renderer.setAnimationLoop(null);
-      mindarThree.stop();
-    }
-  }, []);
+        mindarThree.start();
+        renderer.setAnimationLoop(() => {
+            renderer.render(scene, camera);
+        });
 
-  return (
-    <div style={{width: "100%", height: "100%"}} ref={containerRef}>
-    </div>
-  )
+        return () => {
+            renderer.setAnimationLoop(null);
+            mindarThree.stop();
+        }
+    }, []);
+
+    return (
+        <div style={{width: "100%", height: "100%"}} ref={containerRef}>
+        </div>
+    )
 }
