35a4b81276d1728461939e5ce7256ec0b1ac5c2c
Paloma <paloma.sanchezc@icloud.com>
2025-05-21 14:43:06 +0200

    Description de vos modifications

--- src/App.jsx
+++ src/App.jsx
@@ -123,6 +123,6 @@ export default function AutoStartMindAR() {
   }, []);
   
   return (
-    <div style={{ width: '100vw', height: '100vw' }} ref={containerRef} />
+    <div className="ar-container" ref={containerRef} />
   );
 }
