3087209a654d4c66fa4be27191571d723b02a0ed
Paloma <paloma.sanchezc@icloud.com>
2025-05-22 15:40:37 +0200

    Description de vos modifications

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,26 @@
 import 'aframe';
 import 'mind-ar/dist/mindar-image-aframe.prod.js';
 
-export default function App()
-{
-  return(
+export default function App() {
+  return (
     <>
-      <a-scene mindar-image="imageTargetSrc: /ARimagebased/targets.mind;" vr-mode-ui="enabled: false" device-orientation-permission-ui="enabled: false">
+      <a-scene mindar-image="imageTargetSrc: /ARimagebased/targets-tatoo.mind;" vr-mode-ui="enabled: false" device-orientation-permission-ui="enabled: false">
         <a-camera position="0 0 0" look-controls="enabled: false"></a-camera>
         <a-entity mindar-image-target="targetIndex: 0">
-          <a-sphere 
-            color="black" 
-            radius="0.1" 
+          <a-sphere
+            color="black"
+            radius="0.1"
             position="0 -0.5 0">
+            <a-animation
+              attribute="position"
+              to="0 0.5 0"
+              dur="2000"
+              repeat="indefinite"
+              direction="alternate">
+            </a-animation>
           </a-sphere>
         </a-entity>
       </a-scene>
     </>
-  )
+  );
 }
