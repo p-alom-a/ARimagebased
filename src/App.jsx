e336f65d5151bf7429be6ea923822e581e35a9b4
Paloma <paloma.sanchezc@icloud.com>
2025-05-22 15:34:35 +0200

    Description de vos modifications

--- src/App.jsx
+++ src/App.jsx
@@ -5,10 +5,15 @@ export default function App()
 {
   return(
     <>
-      <a-scene mindar-image="imageTargetSrc: /ARimagebased/targets-tatoo.mind;" vr-mode-ui="enabled: false" device-orientation-permission-ui="enabled: false">
+      <a-scene mindar-image="imageTargetSrc: /ARimagebased/targets.mind;" vr-mode-ui="enabled: false" device-orientation-permission-ui="enabled: false">
         <a-camera position="0 0 0" look-controls="enabled: false"></a-camera>
         <a-entity mindar-image-target="targetIndex: 0">
-          <a-sphere color="black" radius="0.1" position="0 0.4 0"></a-sphere>
+          <a-sphere 
+            color="black" 
+            radius="0.1" 
+            position="0 -0.5 0"
+            animation="property: position; to: 0 0.4 0; dur: 2000; easing: easeOutBounce; delay: 500">
+          </a-sphere>
         </a-entity>
       </a-scene>
     </>
