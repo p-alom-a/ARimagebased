5c240975b57b245a44a61a478783610b2d9fa023
Paloma <paloma.sanchezc@icloud.com>
2025-05-25 18:23:27 +0200

    Description de vos modifications

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,31 @@
-import React from "react";
-import { createRoot } from "react-dom/client";
+import { ARAnchor, ARView } from "react-three-mind";
 
-import { ARView, ARFaceMesh } from "react-three-mind";
- function App() {
+function Plane(props) {
   return (
-      <ARView>
-    <ARFaceMesh>
-      <meshBasicMaterial color="hotpink" wireframe />
-    </ARFaceMesh>
-  </ARView>
-  )
+    <mesh {...props}>
+      <boxGeometry args={[1, 1, 0.1]} />
+      <meshStandardMaterial color="orange" />
+    </mesh>
+  );
+}
 
-
-
-   }
-;
+function App() {
+  return (
+    <ARView
+      imageTargets="https://p-alom-a.github.io/ARimagebased/targets-leUN.mind"
+      filterMinCF={0.001}
+      filterBeta={1000}
+      missTolerance={5}
+      warmupTolerance={5}
+      flipUserCamera={false}
+    >
+      <ambientLight />
+      <pointLight position={[10, 10, 10]} />
+      <ARAnchor target={0}>
+        <Plane />
+      </ARAnchor>
+    </ARView>
+  );
+}
 
 export default App;
