36ab7535b89381f1605718a1815f45b140a617df
Paloma <paloma.sanchezc@icloud.com>
2025-05-25 18:33:23 +0200

    Description de vos modifications

--- src/App.jsx
+++ src/App.jsx
@@ -6,13 +6,21 @@ function Plane(props) {
       <boxGeometry args={[1, 1, 0.1]} />
       <meshStandardMaterial color="orange" />
     </mesh>
-  );
+  );}
+  
+  function PlaneTwo(props) {
+    return (
+      <mesh {...props}>
+        <boxGeometry args={[0.5, 0.5, 0.1]} />
+        <meshStandardMaterial color="blue" />
+      </mesh>
+    );
 }
 
 function App() {
   return (
     <ARView
-      imageTargets="https://p-alom-a.github.io/ARimagebased/targets-leUN.mind"
+      imageTargets="https://p-alom-a.github.io/ARimagebased/targets-multiUN.mind"
       filterMinCF={0.001}
       filterBeta={1000}
       missTolerance={5}
@@ -23,6 +31,9 @@ function App() {
       <pointLight position={[10, 10, 10]} />
       <ARAnchor target={0}>
         <Plane />
+      </ARAnchor>
+      <ARAnchor target={1}>
+        <PlaneTwo />
       </ARAnchor>
     </ARView>
   );
