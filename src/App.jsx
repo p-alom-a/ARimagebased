07c81e364083e8c49756eccd147d98c6b2d754d2
Paloma <paloma.sanchezc@icloud.com>
2025-05-25 18:11:18 +0200

    Description de vos modifications

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,18 @@ import React from "react";
 import { createRoot } from "react-dom/client";
 
 import { ARView, ARFaceMesh } from "react-three-mind";
-
-const container = document.getElementById("root");
-const root = createRoot(container);
-root.render(
-  <ARView>
+ function App() {
+  return (
+      <ARView>
     <ARFaceMesh>
       <meshBasicMaterial color="hotpink" wireframe />
     </ARFaceMesh>
   </ARView>
-);
+  )
+
+
+
+   }
+;
+
+export default App;
