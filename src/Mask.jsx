5cb6435b826c50f8975ba3586d201d484660a2bb
Paloma <paloma.sanchezc@icloud.com>
2025-07-15 20:56:39 +0200

    position

--- src/Mask.jsx
+++ src/Mask.jsx
@@ -31,7 +31,7 @@ export default function App()
                 {/* <!-- Modèle 3D du raton laveur affiché lorsque la cible 0 est détectée --> */}
                 <a-gltf-model
                 rotation="0 -90 0"
-                position="0 -0.25 0"
+                position="0 0.2 0"
                 scale="1 1 1"
                 src="#maskModel"
                 animation-mixer
